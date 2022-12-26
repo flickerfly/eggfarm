bb05c33c94baac88bdd2a33307659cf77dade4d8
Josiah Ritchie <josiah.ritchie@gmail.com>
2022-12-26 16:28:39 +0000

    Pending changes exported from your codespace

---
+++ eggfarm/base.py
@@ -0,0 +1,9 @@
+"""
+eggfarm base module.
+This is the principal module of the eggfarm project.
+here you put your main classes and objects.
+Be creative! do whatever you want!
+"""
+
+# constant variable
+NAME = "eggfarm"
